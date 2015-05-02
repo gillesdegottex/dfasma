a841602585b67932b3861e849707a10726feebc8
Gilles Degottex <gilles.degottex@gmail.com>
2015-05-02 16:47:31 +0100

    #184 done; #263 done

---
+++ src/QSettingsAuto.cpp
@@ -0,0 +1,291 @@
+// Copyright 2007 "Gilles Degottex"
+
+// This file is part of "fmit"
+
+// "Music" is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// "Music" is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#include <iostream>
+using namespace std;
+#include <qsettings.h>
+#include "QSettingsAuto.h"
+#include <qcheckbox.h>
+#include <qspinbox.h>
+#include <qlineedit.h>
+#include <qcombobox.h>
+#include <qgroupbox.h>
+#include <qradiobutton.h>
+#include <qaction.h>
+
+QSettingsAuto::QSettingsAuto(const QString& domain, const QString& product, const QString& setting_version)
+	: QSettings(QSettings::UserScope, domain, product)
+{
+//	beginGroup(QString("/")+product+setting_version+"/");
+    cerr << "QSettingsAuto: " << fileName().toStdString() << endl;
+}
+
+QSettingsAuto::QSettingsAuto()
+{
+//    beginGroup(QString("/")+product+setting_version+"/");
+    cerr << "QSettingsAuto: " << fileName().toStdString() << endl;
+}
+
+void QSettingsAuto::add(QCheckBox* el)
+{
+	assert(el->objectName()!="");
+	m_elements_checkbox.push_back(el);
+    if(contains(el->objectName()))
+        load(el);
+}
+void QSettingsAuto::save(QCheckBox* el)
+{
+    beginGroup("QSettingsAuto/");
+    setValue(el->objectName(), el->isChecked());
+    endGroup();
+}
+void QSettingsAuto::load(QCheckBox* el)
+{
+    beginGroup("QSettingsAuto/");
+    el->setChecked(value(el->objectName(), el->isChecked()).toBool());
+    endGroup();
+}
+
+void QSettingsAuto::add(QSpinBox* el)
+{
+	assert(el->objectName()!="");
+	m_elements_spinbox.push_back(el);
+    if(contains(el->objectName()))
+        load(el);
+}
+void QSettingsAuto::save(QSpinBox* el)
+{
+    beginGroup("QSettingsAuto/");
+    setValue(el->objectName(), el->value());
+    endGroup();
+}
+void QSettingsAuto::load(QSpinBox* el)
+{
+    beginGroup("QSettingsAuto/");
+    el->setValue(value(el->objectName(), el->value()).toInt());
+    endGroup();
+}
+
+void QSettingsAuto::add(QDoubleSpinBox* el)
+{
+    assert(el->objectName()!="");
+    m_elements_doublespinbox.push_back(el);
+    if(contains(el->objectName()))
+        load(el);
+}
+void QSettingsAuto::save(QDoubleSpinBox* el)
+{
+    beginGroup("QSettingsAuto/");
+    setValue(el->objectName(), el->value());
+    endGroup();
+}
+void QSettingsAuto::load(QDoubleSpinBox* el)
+{
+    beginGroup("QSettingsAuto/");
+    el->setValue(value(el->objectName(), el->value()).toDouble());
+    endGroup();
+}
+
+void QSettingsAuto::add(QLineEdit* el)
+{
+    assert(el->objectName()!="");
+    m_elements_lineedit.push_back(el);
+    if(contains(el->objectName()))
+        load(el);
+}
+void QSettingsAuto::save(QLineEdit* el)
+{
+    beginGroup("QSettingsAuto/");
+    setValue(el->objectName(), el->text());
+    endGroup();
+}
+void QSettingsAuto::load(QLineEdit* el)
+{
+    beginGroup("QSettingsAuto/");
+    el->setText(value(el->objectName(), (el->text())).toString());
+    endGroup();
+}
+
+void QSettingsAuto::add(QComboBox* el)
+{
+    assert(el->objectName()!="");
+    m_elements_combobox.push_back(el);
+    if(contains(el->objectName()))
+        load(el);
+}
+void QSettingsAuto::save(QComboBox* el)
+{
+    beginGroup("QSettingsAuto/");
+    setValue(el->objectName(), el->currentIndex());
+    endGroup();
+}
+void QSettingsAuto::load(QComboBox* el)
+{
+    beginGroup("QSettingsAuto/");
+    el->setCurrentIndex(value(el->objectName(), el->currentIndex()).toInt());
+    endGroup();
+}
+
+void QSettingsAuto::add(QGroupBox* el)
+{
+    assert(el->objectName()!="");
+    m_elements_qgroupbox.push_back(el);
+    if(contains(el->objectName()))
+        load(el);
+}
+void QSettingsAuto::save(QGroupBox* el)
+{
+    beginGroup("QSettingsAuto/");
+    setValue(el->objectName(), el->isChecked());
+    endGroup();
+}
+void QSettingsAuto::load(QGroupBox* el)
+{
+    beginGroup("QSettingsAuto/");
+    el->setChecked(value(el->objectName(), el->isChecked()).toBool());
+    endGroup();
+}
+
+void QSettingsAuto::add(QRadioButton* el)
+{
+    assert(el->objectName()!="");
+    m_elements_qradiobutton.push_back(el);
+    if(contains(el->objectName()))
+        load(el);
+}
+void QSettingsAuto::save(QRadioButton* el)
+{
+    beginGroup("QSettingsAuto/");
+    setValue(el->objectName(), el->isChecked());
+    endGroup();
+}
+void QSettingsAuto::load(QRadioButton* el)
+{
+    beginGroup("QSettingsAuto/");
+    el->setChecked(value(el->objectName(), el->isChecked()).toBool());
+    endGroup();
+}
+
+void QSettingsAuto::add(QSlider* el)
+{
+    assert(el->objectName()!="");
+    m_elements_qslider.push_back(el);
+    if(contains(el->objectName()))
+        load(el);
+}
+void QSettingsAuto::save(QSlider* el)
+{
+    beginGroup("QSettingsAuto/");
+    setValue(el->objectName(), el->value());
+    endGroup();
+}
+void QSettingsAuto::load(QSlider* el)
+{
+    beginGroup("QSettingsAuto/");
+    el->setValue(value(el->objectName(), el->value()).toInt());
+    endGroup();
+}
+
+void QSettingsAuto::add(QAction* el)
+{
+    assert(el->objectName()!="");
+    m_elements_qaction.push_back(el);
+    if(contains(el->objectName()))
+        load(el);
+}
+void QSettingsAuto::save(QAction* el)
+{
+    beginGroup("QSettingsAuto/");
+    setValue(el->objectName(), el->isChecked());
+    endGroup();
+}
+void QSettingsAuto::load(QAction* el)
+{
+    beginGroup("QSettingsAuto/");
+    el->setChecked(value(el->objectName(), el->isChecked()).toBool());
+    endGroup();
+}
+
+void QSettingsAuto::saveAll()
+{
+	for(list<QCheckBox*>::iterator it=m_elements_checkbox.begin(); it!=m_elements_checkbox.end(); it++)
+        save(*it);
+
+	for(list<QSpinBox*>::iterator it=m_elements_spinbox.begin(); it!=m_elements_spinbox.end(); it++)
+        save(*it);
+
+    for(list<QDoubleSpinBox*>::iterator it=m_elements_doublespinbox.begin(); it!=m_elements_doublespinbox.end(); it++)
+        save(*it);
+
+	for(list<QLineEdit*>::iterator it=m_elements_lineedit.begin(); it!=m_elements_lineedit.end(); it++)
+        save(*it);
+
+	for(list<QComboBox*>::iterator it=m_elements_combobox.begin(); it!=m_elements_combobox.end(); it++)
+        save(*it);
+
+	for(list<QGroupBox*>::iterator it=m_elements_qgroupbox.begin(); it!=m_elements_qgroupbox.end(); it++)
+        save(*it);
+
+	for(list<QRadioButton*>::iterator it=m_elements_qradiobutton.begin(); it!=m_elements_qradiobutton.end(); it++)
+        save(*it);
+
+    for(list<QSlider*>::iterator it=m_elements_qslider.begin(); it!=m_elements_qslider.end(); it++)
+        save(*it);
+
+    for(list<QAction*>::iterator it=m_elements_qaction.begin(); it!=m_elements_qaction.end(); it++)
+        save(*it);
+
+    sync();
+}
+void QSettingsAuto::loadAll()
+{
+	for(list<QCheckBox*>::iterator it=m_elements_checkbox.begin(); it!=m_elements_checkbox.end(); it++)
+        load(*it);
+
+	for(list<QSpinBox*>::iterator it=m_elements_spinbox.begin(); it!=m_elements_spinbox.end(); it++)
+        load(*it);
+
+    for(list<QDoubleSpinBox*>::iterator it=m_elements_doublespinbox.begin(); it!=m_elements_doublespinbox.end(); it++)
+        load(*it);
+
+	for(list<QLineEdit*>::iterator it=m_elements_lineedit.begin(); it!=m_elements_lineedit.end(); it++)
+        load(*it);
+
+	for(list<QComboBox*>::iterator it=m_elements_combobox.begin(); it!=m_elements_combobox.end(); it++)
+        load(*it);
+
+	for(list<QGroupBox*>::iterator it=m_elements_qgroupbox.begin(); it!=m_elements_qgroupbox.end(); it++)
+        load(*it);
+
+	for(list<QRadioButton*>::iterator it=m_elements_qradiobutton.begin(); it!=m_elements_qradiobutton.end(); it++)
+        load(*it);
+
+    for(list<QSlider*>::iterator it=m_elements_qslider.begin(); it!=m_elements_qslider.end(); it++)
+        load(*it);
+
+    for(list<QAction*>::iterator it=m_elements_qaction.begin(); it!=m_elements_qaction.end(); it++)
+        load(*it);
+
+    sync();
+}
+void QSettingsAuto::clearAll()
+{
+    QSettingsAuto::clear();
+
+    sync();
+}
---
+++ src/QSettingsAuto.h
@@ -0,0 +1,85 @@
+// Copyright 2007 "Gilles Degottex"
+
+// This file is part of "fmit"
+
+// "Music" is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// "Music" is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#ifndef _QSettingsAuto_h_
+#define _QSettingsAuto_h_
+
+#include <assert.h>
+#include <list>
+#include <qsettings.h>
+class QCheckBox;
+class QSpinBox;
+class QDoubleSpinBox;
+class QLineEdit;
+class QComboBox;
+class QGroupBox;
+class QRadioButton;
+class QAction;
+class QSlider;
+
+class QSettingsAuto : public QSettings
+{
+	std::list<QCheckBox*> m_elements_checkbox;
+	std::list<QSpinBox*> m_elements_spinbox;
+    std::list<QDoubleSpinBox*> m_elements_doublespinbox;
+    std::list<QLineEdit*> m_elements_lineedit;
+	std::list<QComboBox*> m_elements_combobox;
+	std::list<QGroupBox*> m_elements_qgroupbox;
+	std::list<QRadioButton*> m_elements_qradiobutton;
+    std::list<QSlider*> m_elements_qslider;
+    std::list<QAction*> m_elements_qaction;
+
+  public:
+    QSettingsAuto(const QString& domain, const QString& product, const QString& setting_version);
+    QSettingsAuto();
+
+	void add(QCheckBox* el);
+    void save(QCheckBox* el);
+    void load(QCheckBox* el);
+    void add(QSpinBox* el);
+    void save(QSpinBox* el);
+    void load(QSpinBox* el);
+    void add(QDoubleSpinBox* el);
+    void save(QDoubleSpinBox* el);
+    void load(QDoubleSpinBox* el);
+    void add(QLineEdit* el);
+    void save(QLineEdit* el);
+    void load(QLineEdit* el);
+    void add(QComboBox* el);
+    void save(QComboBox* el);
+    void load(QComboBox* el);
+    void add(QGroupBox* el);
+    void save(QGroupBox* el);
+    void load(QGroupBox* el);
+    void add(QRadioButton* el);
+    void save(QRadioButton* el);
+    void load(QRadioButton* el);
+    void add(QSlider* el);
+    void save(QSlider* el);
+    void load(QSlider* el);
+    void add(QAction* el);
+    void save(QAction* el);
+    void load(QAction* el);
+
+
+    void saveAll();
+    void loadAll();
+    void clearAll();
+};
+
+#endif // _QSettingsAuto_h_
