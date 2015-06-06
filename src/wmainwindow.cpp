9abea254da425f283053f3bdded6c0c2d5a92262
Gilles Degottex <gilles.degottex@gmail.com>
2015-06-06 13:37:05 +0100

    fix comments

--- src/wmainwindow.cpp
+++ src/wmainwindow.cpp
@@ -1030,7 +1030,6 @@ void WMainWindow::selectedFilesReload() {
     fileInfoUpdate();
 
     if(didanysucceed && reloadSelectedSound) {
-        FLAG
         m_gvWaveform->m_scene->update();
         m_gvAmplitudeSpectrum->updateDFTs();
         m_gvSpectrogram->updateSTFTPlot(true); // Force the STFT computation
