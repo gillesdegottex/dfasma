0d8a2f7bf2a3da0842088d8f48c97f3d2e3703fb
Gilles Degottex <gilles.degottex@gmail.com>
2015-08-24 23:38:09 +0200

    Update package_appveyor_Win64bit.ps1

--- distrib/package_appveyor_Win64bit.ps1
+++ distrib/package_appveyor_Win64bit.ps1
@@ -1,8 +1,9 @@
 #http://doc.qt.io/qt-5/windows-deployment.html
 $VERSION = (git describe --tags --always) | Out-String
+$VERSION -replace "`n|`r"
 echo "Version: $VERSION"
 
-$PACKAGENAME = "DFasma-$VERSION`-Win64bit"
+$PACKAGENAME = "DFasma-$VERSION-Win64bit"
 $QTPATH = "\Qt\5.4\msvc2013_64_opengl"
 
 echo "Packaging $PACKAGENAME"
@@ -11,18 +12,19 @@ echo " "
 New-Item -ItemType directory -Name $PACKAGENAME
 
 # Add the executable
-cp release\dfasma.exe $PACKAGENAME\
+Copy-Item c:\projects\dfasma\release\dfasma.exe $PACKAGENAME
 
 # Add libraries
-cp lib\fftw-3.3.4-dll64\libfftw3-3.dll $PACKAGENAME\
-cp lib\libsndfile-1.0.25-w64\bin\libsndfile-1.dll $PACKAGENAME\
+Copy-Item c:\projects\dfasma\lib\fftw-3.3.4-dll64\libfftw3-3.dll $PACKAGENAME
+Copy-Item c:\projects\dfasma\lib\libsndfile-1.0.25-w64\bin\libsndfile-1.dll $PACKAGENAME
 
 # Add the Qt related libs
 cd $PACKAGENAME\
 #C:/Qt/5.2.1/msvc2012_64_opengl/bin/qtenv2.bat
-export PATH=\C$QTPATH\bin:$PATH
-echo $PATH
-C:$QTPATH\bin\windeployqt.exe --no-translations dfasma.exe
+#export PATH=\C$QTPATH\bin:$PATH
+#echo $PATH
+$env:Path += ";\C$QTPATH\bin"
+c:$QTPATH\bin\windeployqt.exe --no-translations dfasma.exe
 cd ..
 
 # Add the MSVC redistribution installer
