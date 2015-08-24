af80bef8aa5f727f48dd31a56853db792d7ce694
Gilles Degottex <gilles.degottex@gmail.com>
2015-08-24 23:23:50 +0200

    Update package_appveyor_Win64bit.ps1

--- distrib/package_appveyor_Win64bit.ps1
+++ distrib/package_appveyor_Win64bit.ps1
@@ -2,7 +2,7 @@
 $VERSION = (git describe --tags --always) | Out-String
 echo "Version: $VERSION"
 
-$PACKAGENAME = "DFasma-$VERSION'-Win64bit'"
+$PACKAGENAME = "DFasma-${VERSION}-Win64bit"
 $QTPATH = "\Qt\5.4\msvc2013_64_opengl"
 
 echo "Packaging $PACKAGENAME"
