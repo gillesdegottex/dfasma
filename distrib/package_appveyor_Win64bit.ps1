66dd1b6e3a520f1cc499fc842b9e35b2f1b2e5e3
Gilles Degottex <gilles.degottex@gmail.com>
2015-08-25 02:42:06 +0200

    Update package_appveyor_Win64bit.ps1

--- distrib/package_appveyor_Win64bit.ps1
+++ distrib/package_appveyor_Win64bit.ps1
@@ -17,7 +17,7 @@ New-Item -ItemType directory -Name $PACKAGENAME | Out-Null
 Copy-Item c:\projects\dfasma\release\dfasma.exe $PACKAGENAME
 
 # Add libraries
-Copy-Item c:\projects\dfasma\lib\libfftwfile\libfftw3-3.dll $PACKAGENAME
+Copy-Item c:\projects\dfasma\lib\libfftfile\libfftw3-3.dll $PACKAGENAME
 Copy-Item c:\projects\dfasma\lib\libsndfile\bin\libsndfile-1.dll $PACKAGENAME
 
 # Add the Qt related libs
