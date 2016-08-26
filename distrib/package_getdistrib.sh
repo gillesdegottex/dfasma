67a552010c8676c9cdef959dc6025fd4be4f355f
Gilles Degottex <gilles.degottex@gmail.com>
2016-08-27 00:51:27 +0100

    fix for other distrib

--- distrib/package_getdistrib.sh
+++ distrib/package_getdistrib.sh
@@ -2,7 +2,7 @@
 if [[ -n "$1" ]]; then
     DISTRIB=$1
 else
-    DISTRIB=`lsb_release -d |sed 's/Description:\s//g' |sed 's/\s//g' | awk '{print tolower($0)}'`
+    DISTRIB=`lsb_release -d |sed 's/Description:\s//g' |sed 's/\s//g' |sed 's/\..*//g' | awk '{print tolower($0)}'`
 fi
 #if [ "${DISTRIB/Ubuntu}" != "$DISTRIB" ] ; then
 #    DISTRIB=`echo $DISTRIB |sed 's/\(Ubuntu_[0-9]\+\.[0-9]\+\).*/\1/g'`
