a69b336c63fb16a84a0d8084bbd64f55aa6e8dba
Gilles Degottex <gilles.degottex@gmail.com>
2016-10-22 12:47:28 +0100

    Update compile_sdif.sh

--- distrib/compile_sdif.sh
+++ distrib/compile_sdif.sh
@@ -13,16 +13,19 @@ mkdir easdif
 #cvs -d:pserver:anonymous@sdif.cvs.sourceforge.net:/cvsroot/sdif login
 cvs -z3 -d:pserver:anonymous@sdif.cvs.sourceforge.net:/cvsroot/sdif co -P EASDIF_SDIF
 
-# Apply patch for installing static libraries
-cd EASDIF_SDIF
-patch -p0 < ../../../distrib/compile_sdif_install_static.diff
-cd ..
+if [ "$1" = "--static" ]; then
+  # Apply patch for installing static libraries
+  cd EASDIF_SDIF
+  patch -p0 < ../../../distrib/compile_sdif_install_static.diff
+  cd ..
+  SDIFSTATICOPT=-DEASDIF_BUILD_STATIC:BOOL=ON
+fi
 
 # Build path
 mkdir build
 cd build
 echo $PWD/../easdif
-cmake -DSDIF_BUILD_STATIC:BOOL=ON -DEASDIF_BUILD_STATIC:BOOL=ON -DCMAKE_INSTALL_PREFIX_DEFAULTS_INIT:BOOL=ON -DCMAKE_INSTALL_PREFIX:STRING=$PWD/../easdif ../EASDIF_SDIF
+cmake -DSDIF_BUILD_STATIC:BOOL=ON $SDIFSTATICOPT -DCMAKE_INSTALL_PREFIX_DEFAULTS_INIT:BOOL=ON -DCMAKE_INSTALL_PREFIX:STRING=$PWD/../easdif ../EASDIF_SDIF
 make VERBOSE=1
 make install
 ls -l $PWD/../easdif
