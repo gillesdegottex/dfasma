4a6e45f4699ec1a3c0318f71f61d4e5dbfec9a7e
Gilles Degottex <gilles.degottex@gmail.com>
2018-05-13 15:00:28 +0200

    compile EASDIF from submodule

--- distrib/compile_sdif.sh
+++ distrib/compile_sdif.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 
-# Compile Easdif from the sourceforge sources
-# and put everything in the external directory.
+# Compile Easdif from submodule
+# and install everything in external/easdif directory.
 
-rm -fr external/sdif
-mkdir -p external/sdif
+rm -fr external/sdif/easdif
 cd external/sdif
 # Install path
-mkdir easdif
-
-# Sources
-#cvs -d:pserver:anonymous@sdif.cvs.sourceforge.net:/cvsroot/sdif login
-cvs -z3 -d:pserver:anonymous@sdif.cvs.sourceforge.net:/cvsroot/sdif co -P EASDIF_SDIF
-
-# Apply patch for installing static libraries
-cd EASDIF_SDIF
-patch -p0 < ../../../distrib/compile_sdif_install_static.diff
-cd ..
+mkdir -p easdif
 
 if [ "$1" = "--osx" ]; then
   # In order to use libc++ and not libstdc++
@@ -28,6 +18,7 @@ mkdir build
 cd build
 echo $PWD/../easdif
 cmake $OSXOPTS -DSDIF_BUILD_STATIC:BOOL=ON -DEASDIF_BUILD_STATIC:BOOL=ON -DCMAKE_INSTALL_PREFIX_DEFAULTS_INIT:BOOL=ON -DCMAKE_INSTALL_PREFIX:STRING=$PWD/../easdif ../EASDIF_SDIF
-make VERBOSE=1
+# make VERBOSE=1
+make
 make install
 ls -l $PWD/../easdif/*
