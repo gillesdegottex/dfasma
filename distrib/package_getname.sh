d3b61fb5bc7427a17a07e466abd5a523f10f7537
Gilles Degottex <gilles.degottex@gmail.com>
2018-06-17 23:18:07 +0200

    fix

---
+++ distrib/package_getname.sh
@@ -0,0 +1,12 @@
+BINFILE=$1
+
+VERSION=$(bash package_getversion.sh $2)
+
+BINARCH=`objdump -p $BINFILE |grep 'file format' |sed 's/^.*file format\s//g'`
+if [ "${BINARCH/elf64}" = "${BINARCH}" ] ; then
+    ARCH=i386
+else
+    ARCH=amd64
+fi
+
+echo dfasma_$VERSION\_$ARCH
