80937df70433e89b60103e11b4f4cf04b98346c7
Gilles Degottex <gilles.degottex@gmail.com>
2018-06-17 21:54:40 +0200

    fix

--- distrib/package_getname.sh
+++ distrib/package_getname.sh
@@ -2,14 +2,12 @@ BINFILE=$1
 
 VERSION=$(bash package_getversion.sh $2)
 
-echo "Packaging DFasma "$VERSION
 BINARCH=`objdump -p $BINFILE |grep 'file format' |sed 's/^.*file format\s//g'`
 if [ "${BINARCH/elf64}" = "${BINARCH}" ] ; then
     ARCH=i386
 else
     ARCH=amd64
 fi
-echo "Architecture: "$ARCH
 
 PKGNAME=dfasma_$VERSION\_$ARCH
-echo $PKGNAME
+echo $PKGNAME.deb
